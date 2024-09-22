6ef8ca0022ca022d8068d408d172c0898c533abc
xuzongzhen <xzz@outlook.com>
2024-09-22 14:53:43 +0800

    Initial Commit

---
+++ Pomo TAP Watch App/Pomo_TAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomo_TAPApp.swift
+//  Pomo TAP Watch App
+//
+//  Created by 许宗桢 on 2024/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Pomo_TAP_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
